efbb7890df510419820071024832c920a376928a
Arijitnaskar12 <117929311+Arijitnaskar12@users.noreply.github.com>
2023-05-13 20:12:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,20 +8,42 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  let output=arr.forEach(function(element,index,list)
+                {
+    if(arr.marks>50)
+    {
+      console.log(element,index,list);
+    }
+  });
+  
 }
 
 function addData() {
   //Write your code here, just console.log
+  arr.push({id:4,name:"susan",age:"20",marks:45});
+  console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  for(let i=0;i<arr.length;i++){
+  if(arr[i].marks<50){
+    arr.pop();
+  }
+  }
+ console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+let another=[
+  {id:5,name:"Arijit",age:24,marks:50}
+] ;
+ let array3= arr.concat(another);
+  console.log(array3);
 }
