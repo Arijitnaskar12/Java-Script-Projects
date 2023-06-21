794e4525d20096960fddd595deffb8319c3ff9b8
Arijitnaskar12 <117929311+Arijitnaskar12@users.noreply.github.com>
2023-06-21 22:26:38 +0530

    Add files via upload

---
+++ Background Changer/script.js
@@ -0,0 +1,14 @@
+document.getElementById("white").addEventListener("click",()=>{
+    document.body.style.background="white";
+    document.body.style.color="black";
+});
+document.getElementById("aqua").addEventListener("click",()=>{
+    document.body.style.background="aqua";
+});
+document.getElementById("violet").addEventListener("click",()=>{
+    document.body.style.background="violet";
+});
+document.getElementById("black").addEventListener("click",()=>{
+    document.body.style.background="black";
+    document.body.style.color="white";
+});
